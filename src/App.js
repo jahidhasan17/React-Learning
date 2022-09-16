6d338a097eacc681333d94dd2d907251a6b8319c
Jahid_Hassan17 <jahid.hassan@orbitax.com>
2022-09-16 21:21:46 +0600

    dsdf

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { useState } from "react";
-import MyComponent from "./components/MyComponent";
+import ShowCount from "./components/ShowCount";
+import Title from "./components/Title";
+import Button from "./components/Button";
 
 function App() {
 
-    const [show, setShow] = useState(true);
+    const [count1, setCount1] = useState(0);
+    const [count2, setCount2] = useState(0);
+
+    const incrementByOne = () => {
+        setCount1((prevCount) => prevCount + 1);
+    }
+
+    const incrementByFive = () => {
+        setCount2((prevCount) => prevCount + 5);
+    }
 
     return (
         <div className="app">
-            <div>{show && <MyComponent />}</div>
-            <p>
-                <button type="button" onClick={() => setShow((prevShow) => !prevShow)}>
-                    {show ? 'Hide post' : 'Show post'}
-                </button>
-            </p>
+            <Title />
+            <ShowCount count={count1} title={"Counter 1"}/>
+            <Button handleClick={incrementByOne}>Increment by one</Button>
+            <hr />
+            <ShowCount count={count2} title="Counter 2" />
+            <Button handleClick={incrementByFive}>Increment by five</Button>
         </div>
     );
 }
