6cf90f7a6b2f67b129448109ff510c4b3c6bf0c7
Jahid_Hassan17 <jahid.hassan@orbitax.com>
2022-09-16 22:10:13 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import ShowCount from "./components/ShowCount";
 import Title from "./components/Title";
 import Button from "./components/Button";
@@ -8,18 +8,26 @@ function App() {
     const [count1, setCount1] = useState(0);
     const [count2, setCount2] = useState(0);
 
-    const incrementByOne = () => {
+    const incrementByOne = useCallback(() => {
         setCount1((prevCount) => prevCount + 1);
-    }
+    }, []);
 
-    const incrementByFive = () => {
+    const incrementByFive = useCallback(() => {
         setCount2((prevCount) => prevCount + 5);
-    }
+    }, []);
+
+    const isEvenOrOdd = useMemo(() => {
+        console.log("called isEvenOrOdd");
+        let i = 0;
+        while (i < 1000000000) i += 1; // costly operation
+        return count1 % 2 === 0;
+    },[count1]);
 
     return (
         <div className="app">
             <Title />
             <ShowCount count={count1} title={"Counter 1"}/>
+            <span>{isEvenOrOdd ? 'Even' : 'Odd'}</span>
             <Button handleClick={incrementByOne}>Increment by one</Button>
             <hr />
             <ShowCount count={count2} title="Counter 2" />
