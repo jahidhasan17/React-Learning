248d7c65ad1acb65aa8777b358efb37c60e42315
Jahid_Hassan17 <jahid.hassan@orbitax.com>
2022-09-23 21:45:48 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import { useState } from "react";
-import LayoutComponentOne from "./components/LayoutComponentOne";
-import LayoutComponentTwo from "./components/LayoutComponentTwo";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export default function App() {
 
-    const [isOne, setOne] = useState(true);
-
     return (
-        <div className="app">
-            {isOne ? <LayoutComponentOne /> : <LayoutComponentTwo />}
-            <button onClick={() => setOne(!isOne)}>{isOne ? "Hide One" : "Show One"}</button>
-        </div>
+        <Container>
+            <Row>
+                <Col>
+                    <Button variant="primary">Primary</Button>
+                </Col>
+                <Col>
+                    <Button variant="danger">Danger</Button>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a longer card with supporting text below as a natural
+                                lead-in to additional content. This content is a little bit
+                                longer.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
 }
